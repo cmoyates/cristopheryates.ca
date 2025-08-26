0f305f6c063d1441f9857e92375038e17672758e
cmoyates <cmoyates@gmail.com>
2025-08-26 00:49:01 -0230

    Added shadcn/ui and react integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,10 +3,15 @@ import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
 import cloudflare from "@astrojs/cloudflare";
 
+import react from "@astrojs/react";
+
 // https://astro.build/config
 export default defineConfig({
   adapter: cloudflare(),
+
   vite: {
     plugins: [tailwindcss()],
   },
+
+  integrations: [react()],
 });
